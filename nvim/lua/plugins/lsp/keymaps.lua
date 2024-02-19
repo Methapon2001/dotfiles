b4dbd5e069bacb44582cc0648f7df6ceef7a774f
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-02-19 17:49:29 +0700

    Conditional trigger mini.extra (lsp picker)

--- nvim/lua/plugins/lsp/keymaps.lua
+++ nvim/lua/plugins/lsp/keymaps.lua
@@ -1,16 +1,55 @@
 M = {}
 
 ---@type LazyKeysSpec[]
--- stylua: ignore
 M.keys = {
   { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
   { "<leader>cr", vim.lsp.buf.rename, desc = "Rename" },
   { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action" },
   { "<leader>cd", vim.diagnostic.open_float, desc = "Line Diagnostics" },
-  { "gD", function() require("mini.extra").pickers.lsp({ scope = "declaration" }, { reuse_win = true }) end, desc = "Goto Declaration" },
-  { "gd", function() require("mini.extra").pickers.lsp({ scope = "definition" }, { reuse_win = true }) end, desc = "Goto Definition" },
-  { "gi", function() require("mini.extra").pickers.lsp({ scope = "implementation" }, { reuse_win = true }) end, desc = "Goto Implementation" },
-  { "gr", function() require("mini.extra").pickers.lsp({ scope = "references" }, { reuse_win = true }) end, desc = "Goto References" },
+  {
+    "gD",
+    function()
+      vim.lsp.buf.declaration({
+        on_list = function(opts)
+          if #opts.items > 1 then
+            require("mini.extra").pickers.lsp({ scope = "declaration" }, { reuse_win = true })
+          else
+            vim.lsp.buf.declaration()
+          end
+        end,
+      })
+    end,
+    desc = "Goto Declaration",
+  },
+  {
+    "gd",
+    function()
+      vim.lsp.buf.definition({
+        on_list = function(opts)
+          if #opts.items > 1 then
+            require("mini.extra").pickers.lsp({ scope = "definition" }, { reuse_win = true })
+          else
+            vim.lsp.buf.definition()
+          end
+        end,
+      })
+    end,
+    desc = "Goto Definition",
+  },
+  {
+    "gi",
+    function()
+      require("mini.extra").pickers.lsp({ scope = "implementation" }, { reuse_win = true })
+    end,
+    desc = "Goto Implementation",
+  },
+  {
+    "gr",
+    function()
+      require("mini.extra").pickers.lsp({ scope = "references" }, { reuse_win = true })
+    end,
+    desc = "Goto References",
+  },
   { "K", vim.lsp.buf.hover, desc = "Hover" },
   { "]d", vim.diagnostic.goto_next, desc = "Next Diagnostic" },
   { "[d", vim.diagnostic.goto_prev, desc = "Prev Diagnostic" },
