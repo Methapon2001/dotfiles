6a475fef73ccf4a7182653b45594a3e210d5bbc7
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-06-20 14:14:58 +0700

    Update Rust LSP setting

--- nvim/lua/plugins/lsp/lang/rust.lua
+++ nvim/lua/plugins/lsp/lang/rust.lua
@@ -6,9 +6,7 @@ return {
         "Saecki/crates.nvim",
         event = { "BufRead Cargo.toml" },
         opts = {
-          src = {
-            cmp = { enabled = true },
-          },
+          completion = { cmp = { enabled = true } },
         },
       },
     },
