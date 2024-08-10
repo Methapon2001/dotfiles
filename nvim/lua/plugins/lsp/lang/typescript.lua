4b4747972fc5acb9145325943aff552317dc5545
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-08-10 15:10:04 +0700

    Adjust to support volar 2.x.x

--- nvim/lua/plugins/lsp/lang/typescript.lua
+++ nvim/lua/plugins/lsp/lang/typescript.lua
@@ -4,6 +4,25 @@ return {
     opts = {
       servers = {
         tsserver = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+          },
+          init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = vim.env.MASON
+                  or (vim.fn.stdpath("data") .. "/mason")
+                    .. "/packages/vue-language-server/node_modules/@vue/language-server",
+                languages = { "vue" },
+                configNamespace = "typescript",
+                enableForWorkspaceTypeScriptVersions = true,
+              },
+            },
+          },
           settings = {
             typescript = {
               inlayHints = {
