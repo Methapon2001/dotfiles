cbc10e07311620477bd6e04dfef460ce8da36200
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-09-08 16:05:20 +0700

    Remove comment

--- nvim/lua/plugins/lsp/lang/typescript.lua
+++ nvim/lua/plugins/lsp/lang/typescript.lua
@@ -48,64 +48,6 @@ return {
             },
           },
         },
-        -- tsserver = {
-        --   filetypes = {
-        --     "javascript",
-        --     "javascriptreact",
-        --     "typescript",
-        --     "typescriptreact",
-        --   },
-        --   init_options = {
-        --     plugins = {
-        --       {
-        --         name = "@vue/typescript-plugin",
-        --         location = vim.env.MASON
-        --           or (vim.fn.stdpath("data") .. "/mason")
-        --             .. "/packages/vue-language-server/node_modules/@vue/language-server",
-        --         languages = { "vue" },
-        --         configNamespace = "typescript",
-        --         enableForWorkspaceTypeScriptVersions = true,
-        --       },
-        --     },
-        --   },
-        --   settings = {
-        --     typescript = {
-        --       inlayHints = {
-        --         includeInlayEnumMemberValueHints = true,
-        --         includeInlayFunctionLikeReturnTypeHints = true,
-        --         includeInlayFunctionParameterTypeHints = true,
-        --         includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-        --         includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        --         includeInlayPropertyDeclarationTypeHints = true,
-        --         includeInlayVariableTypeHints = false,
-        --       },
-        --       format = {
-        --         indentSize = vim.o.shiftwidth,
-        --         convertTabsToSpaces = vim.o.expandtab,
-        --         tabSize = vim.o.tabstop,
-        --       },
-        --     },
-        --     javascript = {
-        --       inlayHints = {
-        --         includeInlayEnumMemberValueHints = true,
-        --         includeInlayFunctionLikeReturnTypeHints = true,
-        --         includeInlayFunctionParameterTypeHints = true,
-        --         includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-        --         includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        --         includeInlayPropertyDeclarationTypeHints = true,
-        --         includeInlayVariableTypeHints = false,
-        --       },
-        --       format = {
-        --         indentSize = vim.o.shiftwidth,
-        --         convertTabsToSpaces = vim.o.expandtab,
-        --         tabSize = vim.o.tabstop,
-        --       },
-        --     },
-        --     completions = {
-        --       completeFunctionCalls = true,
-        --     },
-        --   },
-        -- },
       },
     },
   },
