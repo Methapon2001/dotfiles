c9a2a997e6900d2ea02f40da1b4e3798e5f43abf
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-09-08 10:14:56 +0700

    Add fish shell config

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+zoxide init --cmd cd fish | source
+starship init fish | source
+
+alias pn="pnpm"
+alias pnx="pnpx"
+alias ls="lsd"
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
