0d7ae38a44a47422be4a44dd90c6b01b1cb9e41d
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-10-03 20:36:11 +0700

    Add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrains Mono Nerd Font Mono")
+config.font_size = 11.5
+config.color_scheme = "Catppuccin Macchiato"
+
+config.enable_tab_bar = false
+config.window_background_opacity = 0.94
+
+config.window_padding = {
+	left = 24,
+	right = 24,
+	top = 8,
+	bottom = 8,
+}
+
+return config
