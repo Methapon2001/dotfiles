674451177d1d7eb512132c7b9cf21dc61678da78
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-10-05 13:27:52 +0700

    Update wezterm padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,10 +10,10 @@ config.enable_tab_bar = false
 config.window_background_opacity = 0.94
 
 config.window_padding = {
-	left = 24,
-	right = 24,
-	top = 8,
-	bottom = 8,
+  left = 24,
+  right = 24,
+  top = 24,
+  bottom = 24,
 }
 
 return config
