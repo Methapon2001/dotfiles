0f89eab5a957e00fb55369ed6c120aa9844cc711
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-10-12 09:34:37 +0700

    Use blink.cmp instead of nvim-cmp

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -37,8 +37,6 @@ return {
         { "folke/neodev.nvim", opts = {} },
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
       },
     },
     opts = {
@@ -90,14 +88,9 @@ return {
       })
 
       local servers = opts.servers
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-      local capabilities = vim.tbl_deep_extend(
-        "force",
-        opts.capabilities or {},
-        vim.lsp.protocol.make_client_capabilities(),
-        cmp_nvim_lsp.default_capabilities()
-      )
+      local capabilities =
+        vim.tbl_deep_extend("force", opts.capabilities or {}, vim.lsp.protocol.make_client_capabilities())
 
       local function setup(server)
         local server_opts = vim.tbl_deep_extend("force", {
--- nvim/lua/plugins/lsp/lang/rust.lua
+++ nvim/lua/plugins/lsp/lang/rust.lua
@@ -1,28 +1,15 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "Saecki/crates.nvim",
-        event = { "BufRead Cargo.toml" },
-        opts = {
-          completion = { cmp = { enabled = true } },
-          lsp = {
-            enabled = true,
-            actions = true,
-            completion = true,
-            hover = true,
-          },
-        },
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      lsp = {
+        enabled = true,
+        actions = true,
+        completion = true,
+        hover = true,
       },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-        { name = "crates" },
-      }))
-    end,
   },
   {
     "neovim/nvim-lspconfig",
