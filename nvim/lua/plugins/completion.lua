0dd1a8a59eb5263ac350e69ee47816334d6f657d
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2024-11-29 19:52:35 +0700

    Update blink.cmp to latest commit

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -8,8 +8,8 @@ return {
       keymap = {
         preset = "enter",
       },
-      windows = {
-        autocomplete = {
+      completion = {
+        menu = {
           draw = {
             columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
           },
@@ -17,12 +17,6 @@ return {
         documentation = {
           auto_show = true,
         },
-      },
-      highlight = {
-        -- fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release
-        use_nvim_cmp_as_default = true,
       },
     },
   },
