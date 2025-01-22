172178c23c69a319d171e3d7b0ca1695aa939f6c
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-01-22 19:00:48 +0700

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,12 +3,16 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrains Mono Nerd Font Mono")
-config.font_size = 11.5
-config.line_height = 1.2
+config.font_size = 11
+config.line_height = 1.25
 config.color_scheme = "Catppuccin Macchiato"
 config.initial_cols = 120
-config.initial_rows = 30
+config.initial_rows = 24
 config.max_fps = 120
+config.default_cursor_style = "BlinkingBlock"
+config.animation_fps = 1
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 
 config.enable_tab_bar = false
 
