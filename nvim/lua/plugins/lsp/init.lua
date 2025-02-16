515430d9c0ae3826a47e3236febb2b71e9799884
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-02-16 15:47:16 +0700

    Update lspconfig for neovim 0.11

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -21,12 +21,19 @@ return {
     opts = {
       diagnostics = {
         underline = true,
-        update_in_insert = true,
+        update_in_insert = false,
         virtual_text = {
           spacing = 4,
           source = "if_many",
         },
-        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.HINT] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+          },
+        },
       },
       servers = {
         lua_ls = {
