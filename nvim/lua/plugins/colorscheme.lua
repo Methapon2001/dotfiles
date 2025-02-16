55fd334a311a2a5e2f7d7bc82d08e5c5f2a1e720
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-02-16 14:32:09 +0700

    Add neovim colorscheme integrations

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,33 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  opts = { flavour = "macchiato" },
+  opts = {
+    flavour = "macchiato",
+    integrations = {
+      blink_cmp = true,
+      fidget = true,
+      gitsigns = true,
+      indent_blankline = {
+        enabled = true,
+        scope_color = "lavender",
+        colored_indent_levels = false,
+      },
+      mason = true,
+      mini = {
+        enabled = true,
+        indentscope_color = "lavender",
+      },
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      treesitter = true,
+      treesitter_context = true,
+    },
+  },
 }
