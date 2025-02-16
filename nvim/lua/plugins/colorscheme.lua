2e02a27da7b70c4b16360f79b3482758deb85a44
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-02-16 20:27:43 +0700

    Change neovim undercurl to underline

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -20,10 +20,10 @@ return {
       native_lsp = {
         enabled = true,
         underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
         },
       },
       treesitter = true,
