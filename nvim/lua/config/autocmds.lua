dc9ba1c2f917418ad80232efa242c430ca6f5e63
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-02-17 20:25:45 +0700

    Add neovim autocmd restore cursor position

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,27 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+autocmd("TextYankPost", {
+  group = augroup("highlight_yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+autocmd({ "BufRead" }, {
+  group = augroup("RestoreCursor", {}),
+  pattern = "*",
+  callback = function()
+    if vim.buftype == "terminal" then
+      return
+    end
+    local ft = vim.bo.filetype
+    if
+      not (ft:match("commit") or ft:match("rebase"))
+      and vim.fn.line("'\"") > 1
+      and vim.fn.line("'\"") <= vim.fn.line("$")
+    then
+      vim.cmd('normal! g`"')
+    end
   end,
 })
