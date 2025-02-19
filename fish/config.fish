715f5cd6385f93724ab0c35a766c81ab3386dfd8
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-02-19 20:40:17 +0700

    Change lsd to eza instead

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,6 @@ set -gx EDITOR nvim
 zoxide init --cmd cd fish | source
 starship init fish | source
 
-alias ls="lsd"
-alias l="ls -l"
-alias la="ls -a"
-alias lla="ls -la"
-alias lt="ls --tree"
+alias ls="eza --group-directories-first --icons always"
+alias l="eza --group-directories-first -l"
+alias la="eza --group-directories-first -a"
