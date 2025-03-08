d1356cda7555a9635355631b686e7b9a36a0769e
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-03-08 10:08:28 +0700

    Add path

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,14 @@ starship init fish | source
 alias ls="eza --group-directories-first --icons always"
 alias l="eza --group-directories-first -l"
 alias la="eza --group-directories-first -a"
+
+# cargo
+set -gx PATH $HOME/.cargo/bin $PATH
+# cargo end
+
+# pnpm
+set -gx PNPM_HOME "/home/methapon2001/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
