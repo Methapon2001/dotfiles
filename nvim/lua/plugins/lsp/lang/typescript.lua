4e6ce363d73285726aa410f23b9b9f9725246218
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-03-13 20:47:26 +0700

    Make vue neovim config independent from typescript

--- nvim/lua/plugins/lsp/lang/typescript.lua
+++ nvim/lua/plugins/lsp/lang/typescript.lua
@@ -36,17 +36,6 @@ return {
               },
             },
           },
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = vim.env.MASON
-                or (vim.fn.stdpath("data") .. "/mason")
-                  .. "/packages/vue-language-server/node_modules/@vue/language-server",
-              languages = { "vue" },
-              configNamespace = "typescript",
-              enableForWorkspaceTypeScriptVersions = true,
-            },
-          },
         },
       },
     },
--- nvim/lua/plugins/lsp/lang/vue.lua
+++ nvim/lua/plugins/lsp/lang/vue.lua
@@ -1,14 +1,33 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      volar = {
-        init_options = {
-          vue = {
-            hybridMode = false,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        volar = {
+          init_options = {
+            vue = {
+              hybridMode = false,
+            },
           },
         },
+        vtsls = {},
       },
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      if type(opts.servers.vtsls.plugins) ~= "table" then
+        opts.servers.vtsls.plugins = {}
+      end
+
+      vim.list_extend(opts.servers.vtsls.plugins, {
+        name = "@vue/typescript-plugin",
+        location = require("utils").get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+        languages = { "vue" },
+        configNamespace = "typescript",
+        enableForWorkspaceTypeScriptVersions = true,
+      })
+    end,
   },
 }
