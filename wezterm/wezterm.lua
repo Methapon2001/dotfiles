ef8c7f47cfc7e4501d91d69dc196edc7515d0ccc
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-03-15 20:58:04 +0700

    Rearrange wezter mconfig

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,14 +2,16 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.color_scheme = "Catppuccin Macchiato"
+
+-- font
 config.font = wezterm.font_with_fallback({ "JetBrains Mono Nerd Font Mono", "Noto Sans Thai" })
 config.font_size = 11.5
 config.line_height = 1.25
-config.color_scheme = "Catppuccin Macchiato"
+
+-- ui
+config.enable_tab_bar = false
 config.initial_cols = 120
 config.initial_rows = 24
-config.max_fps = 165
-
-config.enable_tab_bar = false
 
 return config
