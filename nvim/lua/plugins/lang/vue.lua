e476d275a3af03812f973d178d1913a0cc0f7ab8
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-04-20 13:39:05 +0700

    Adjust vue lsp to use hybrid mode

--- nvim/lua/plugins/lang/vue.lua
+++ nvim/lua/plugins/lang/vue.lua
@@ -6,7 +6,7 @@ return {
         volar = {
           init_options = {
             vue = {
-              hybridMode = false,
+              hybridMode = true,
             },
           },
         },
@@ -17,16 +17,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      if type(opts.servers.vtsls.plugins) ~= "table" then
-        opts.servers.vtsls.plugins = {}
-      end
-
-      vim.list_extend(opts.servers.vtsls.plugins, {
-        name = "@vue/typescript-plugin",
-        location = require("utils").get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-        languages = { "vue" },
-        configNamespace = "typescript",
-        enableForWorkspaceTypeScriptVersions = true,
+      table.insert(opts.servers.vtsls.filetypes, "vue")
+      require("utils").extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = require("utils").get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        },
       })
     end,
   },
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -9,4 +9,25 @@ function M.get_pkg_path(pkg, path)
   return (vim.env.MASON or (vim.fn.stdpath("data") .. "/mason")) .. "/packages/" .. pkg .. "/" .. (path or "")
 end
 
+--- This extends a deeply nested list with a key in a table
+--- that is a dot-separated string.
+--- The nested list will be created if it does not exist.
+---@generic T
+---@param base T[]
+---@param key string
+---@param values T[]
+---@return T[]?
+function M.extend(base, key, values)
+  local keys = vim.split(key, ".", { plain = true })
+  for i = 1, #keys do
+    local k = keys[i]
+    base[k] = base[k] or {}
+    if type(base) ~= "table" then
+      return
+    end
+    base = base[k]
+  end
+  return vim.list_extend(base, values)
+end
+
 return M
