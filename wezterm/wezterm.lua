a99feaaa499cf7143b685d47567f8180d1390705
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-05-27 20:51:07 +0700

    Reduce wezterm line height

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ config.color_scheme = "Catppuccin Macchiato"
 -- font
 config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font Mono", "Noto Sans Thai" })
 config.font_size = 11.5
-config.line_height = 1.25
+config.line_height = 1.15
 
 -- ui
 config.enable_tab_bar = false
