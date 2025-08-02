303ee1b89c031216347bae3ffd422c075c7e40ec
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-08-02 11:09:03 +0700

    Fix nvim native lsp does not support on_new_config

--- nvim/lua/plugins/lang/json.lua
+++ nvim/lua/plugins/lang/json.lua
@@ -7,9 +7,8 @@ return {
     opts = {
       servers = {
         jsonls = {
-          on_new_config = function(conf)
-            conf.settings.json.schemas = conf.settings.json.schemas or {}
-            vim.list_extend(conf.settings.json.schemas, require("schemastore").json.schemas())
+          before_init = function(_, conf)
+            require("utils").extend(conf, "settings.json.schemas", require("schemastore").json.schemas())
           end,
           settings = {
             json = {
