1eece7cbbd6d92a3f3a86ccf94095a59b835ce6d
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-09-03 10:46:36 +0700

    Enable csharp formatter

--- nvim/lua/plugins/lang/csharp.lua
+++ nvim/lua/plugins/lang/csharp.lua
@@ -1,8 +1,17 @@
 return {
   {
     "seblyng/roslyn.nvim",
+    ft = "cs",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
     opts = {},
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["cs"] = { "csharpier" },
+      },
+    },
   },
 }
