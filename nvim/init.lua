90abf66418e4e480418d73767fe0f542e3c4f8f2
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-09-18 09:23:24 +0700

    Refactor nvim open dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -14,6 +14,19 @@ vim.opt.rtp:prepend(lazypath)
 require("config.options")
 require("config.autocmds")
 require("config.keymaps")
+
+if vim.fn.argc() == 1 then
+  local argv = vim.fn.argv(0)
+
+  if type(argv) ~= "string" then
+    return
+  end
+
+  local stat = vim.uv.fs_stat(argv)
+  if stat and stat.type == "directory" then
+    vim.fn.chdir(argv)
+  end
+end
 
 require("lazy").setup({
   spec = {
--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -6,19 +6,6 @@ return {
   init = function()
     --- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
     vim.g.loaded_netrw = 1
-
-    if vim.fn.argc() == 1 then
-      local argv = vim.fn.argv(0)
-
-      if type(argv) ~= "string" then
-        return
-      end
-
-      local stat = vim.uv.fs_stat(argv)
-      if stat and stat.type == "directory" then
-        vim.fn.chdir(argv)
-      end
-    end
   end,
   keys = {
     {
