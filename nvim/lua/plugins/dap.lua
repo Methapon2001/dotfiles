90f935c2b6ad8f47580c6c30959c41b17087d98e
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-12-09 19:23:41 +0700

    Change nvim dap ui to dap-view

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -2,21 +2,29 @@
 ---@type LazySpec|LazySpec[]
 return {
   {
+    "igorlfs/nvim-dap-view",
+    lazy = false,
+    ---@module 'dap-view'
+    ---@type dapview.Config
+    opts = {
+      auto_toggle = true,
+    },
+  },
+  {
     "mfussenegger/nvim-dap",
     dependencies = {
-      { "rcarriga/nvim-dap-ui" },
+      { "igorlfs/nvim-dap-view" },
       { "theHamsta/nvim-dap-virtual-text", opts = {} },
     },
     -- stylua: ignore
     keys = {
       { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
       { "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
-      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
-      { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-      { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
-      { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
-      { "<leader>ds", function() require("dap").session() end, desc = "Session" },
       { "<leader>dt", function() require("dap").terminate({ all = true }) end, desc = "Terminate" },
+      { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "Terminate" },
+      { "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<S-F11>", function() require("dap").step_out() end, desc = "Step Out" },
     },
     config = function()
       vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
@@ -32,28 +40,6 @@ return {
           "Dap" .. name,
           { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
         )
-      end
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "nvim-neotest/nvim-nio" },
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "<leader>du", function() require("dapui").toggle() end, desc = "Dap UI" },
-      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
-    },
-    config = function(_, opts)
-      local dap, dapui = require("dap"), require("dapui")
-
-      dapui.setup(opts)
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
       end
     end,
   },
